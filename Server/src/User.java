e67f6b3a24b090288a95f5ce7bca063863637512
Irem Yildiz <irem.yildiz@polymtl.ca>
2024-02-13 16:56:21 -0500

    Correction de fichier gson&ajout de la classe UserDataBase

--- Server/src/User.java
+++ Server/src/User.java
@@ -1,4 +1,17 @@
 public class User {
-    private String username;
-    private String password;
+    private final String username;
+    private final String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
---
+++ Server/src/UserDataBase.java
@@ -0,0 +1,5 @@
+import com.google.gson.Gson;
+
+public class UserDataBase {
+    private static final String User_BD_File = "UserBD.gson";
+}
