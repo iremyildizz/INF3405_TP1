19bfc2df459ee65bc33e5d186785056be854f0f9
Irem Yildiz <irem.yildiz@polymtl.ca>
2024-02-13 17:24:03 -0500

    code n'est pas fini alors il y a des erreurs

--- Server/src/UserDataBase.java
+++ Server/src/UserDataBase.java
@@ -1,5 +1,15 @@
 import com.google.gson.Gson;
 
+import java.util.List;
+
 public class UserDataBase {
-    private static final String User_BD_File = "UserBD.gson";
+    private static final String User_DB_File = "UserBD.gson";
+    private List<User> users;
+    private Gson gson = new Gson();
+
+    public UserDataBase() {
+        users = loadUsers();
+    }
+
+    private List<User> loadUsers(){}
 }
