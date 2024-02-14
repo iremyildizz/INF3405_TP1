5b2e82bdbc2a6604f9be1df0949f09ec4dd1a320
Irem Yildiz <irem.yildiz@polymtl.ca>
2024-02-13 20:47:30 -0500

    Fonctions added

--- Server/src/UserDataBase.java
+++ Server/src/UserDataBase.java
@@ -1,9 +1,15 @@
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDataBase {
-    private static final String User_DB_File = "UserBD.gson";
+    private static final String Data_Base_File = "UserBD.gson";
     private List<User> users;
     private Gson gson = new Gson();
 
@@ -11,5 +17,28 @@ public class UserDataBase {
         users = loadUsers();
     }
 
-    private List<User> loadUsers(){}
+    private List<User> loadUsers() {
+        try(FileReader reader = new FileReader(Data_Base_File)) {
+            Type userlistType = new TypeToken<ArrayList<User>>(){}.getType();
+            return gson.fromJson(reader, userlistType);
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public void addUser(User user){
+        users.add(user);
+    }
+
+    private void saveUserToDataBase(){
+        try(FileWriter writer = new FileWriter(Data_Base_File)) {
+            gson.toJson(users, writer);
+        } catch(IOException e){
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+    }
+
+    public boolean validateUser(String username, String password){
+        return users.stream().anyMatch(user -> user.getUsername().equals(username) && user.getPassword().equals(password));
+    }
 }
